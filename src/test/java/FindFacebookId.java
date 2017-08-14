1cd29e4a6c274c76de7f0ef1dfdbfcd77bc5f689
IvanLihoy <Lihoy92ivan@gmail.com>
2017-08-15 00:17:06 +0300

    api test

--- src/test/java/FindFacebookId.java
+++ src/test/java/FindFacebookId.java
@@ -1,6 +1,7 @@
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -11,10 +12,11 @@ public class FindFacebookId {
     public static void main(String[] args) throws IOException {
 
         CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpResponse response = httpclient.execute(new HttpGet("https://www.7xter.com/ads/findmyid/index.php?username=joe.colella.397?fref=grp_mmbr_list.html"));
+        HttpResponse response = httpclient.execute(new HttpGet("http://soft.it-hillel.com.ua:3000/api/users"));
         HttpEntity entity = response.getEntity();
         String responseString = EntityUtils.toString(entity, "UTF-8");
-        System.out.println(responseString.replaceAll("[a-zA-Z\\s]", ""));
+        System.out.println(responseString);
+        //System.out.println(responseString.replaceAll("[a-zA-Z\\s]", ""));
     }
 
 
